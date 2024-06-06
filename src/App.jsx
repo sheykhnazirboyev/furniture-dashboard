4f65278a021e3c49bda67adc09a04ce7bc9ce5ad
sheykhnazirboyev <sheykhnazirboyev@gmail.com>
2024-06-06 16:35:40 +0500

    feat: added react router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
 
-  return <>Furnitre</>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/register" element={<Register />} />
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App;
