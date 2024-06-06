aab3406d943ca85d005dc2f6e7dec2377d32a615
sheykhnazirboyev <sheykhnazirboyev@gmail.com>
2024-06-06 16:56:03 +0500

    feat: add dashboard to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import GeneralLayout from "./pages/GeneralLayout";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
-
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/register" element={<Register />} />
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GeneralLayout />} >
+          <Route index element={<Home />} />
+          <Route path="/category" element={<CategoryPage />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
